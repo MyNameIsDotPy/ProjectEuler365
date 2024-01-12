2e3f07b4d383d1103862b2974c1f9c2def8df0b9
__init__ <49379301+MyNameIsDotPy@users.noreply.github.com>
2024-01-12 16:14:04 -0500

    Reto19

--- cpp/ProjectEulerCpp/ProjectEulerCpp/main.cpp
+++ cpp/ProjectEulerCpp/ProjectEulerCpp/main.cpp
@@ -3,6 +3,6 @@
 #include "iostream"
 int main()
 {
-    std::cout << reto18();
+    std::cout << reto19();
     return 0;
 }
---
+++ cpp/ProjectEulerCpp/ProjectEulerCpp/reto19.cpp
@@ -0,0 +1,58 @@
+﻿#include "reto19.h"
+
+int reto19()
+{
+    int day = 0;
+    int month = 0;
+    int weekDay = 0;
+    int year = 1900;
+    int months[] = {
+        31,
+        28,
+        31,
+        30,
+        31,
+        30,
+        31,
+        31,
+        30,
+        31,
+        30,
+        31
+    };
+    int sundayCount = 0;
+    while(year<=2000)
+    {
+        bool isLeap = year%4==0 && (year%100!=0 || year%400==0);
+        if(year>=1901)
+        {
+            if(weekDay == 6 && day == 0)
+            {
+                sundayCount++;
+            }            
+        }
+        weekDay = (weekDay+1)%7;
+        if(isLeap)
+        {
+            months[1] = 29;
+        }
+        else
+        {
+            months[1] = 28;   
+        }
+        
+        if(day>=months[month]-1)
+        {
+            month++;
+            day = 0;
+            if(month >= 12)
+            {
+                month = 0;
+                year++;
+            }
+            continue;
+        }
+        day++;
+    }
+    return sundayCount;
+}
